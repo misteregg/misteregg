e33857ffb48086d2a00980b910b503fae797672a
Guilherme Pressutto <gpressutto5@gmail.com>
2019-01-26 18:27:42 -0200

    Added interactable object

---
+++ Assets/Platform/Object/ObjectBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Object
+{
+    public class ObjectBehaviour : MonoBehaviour
+    {
+        public GameObject interactButton;
+
+        private void Start()
+        {
+            interactButton.SetActive(false);
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            interactButton.SetActive(true);
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            interactButton.SetActive(false);
+        }
+    }
+}
