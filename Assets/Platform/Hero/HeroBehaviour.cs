e4d0d2586626613daa1f4739bd4c74398bb2e5ae
Roger Cruz <spark.crz@gmail.com>
2019-01-26 05:53:45 -0200

    Add flappy ass birdesque behavior stuff for the lulz

---
+++ Assets/Platform/Hero/HeroBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Platform.Hero
+{
+    public class HeroBehaviour : MonoBehaviour
+    {
+        public float speed = 5;
+        private Rigidbody2D rb2d;
+        private UserInput input;
+
+        private float horizontalPush = 0;
+        private float verticalPush = 0;
+
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            input = new UserInput(this);
+        }
+
+        void FixedUpdate()
+        {
+            input.Update();
+
+            float moveVertical = verticalPush > 0
+                ? verticalPush
+                : rb2d.velocity.y;
+            float moveHorizontal = horizontalPush;
+
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rb2d.velocity = movement;
+
+            verticalPush = 0;
+            horizontalPush = 0;
+            moveHorizontal = 0;
+        }
+
+        public void Walk(float moveHorizontal)
+        {
+            horizontalPush = moveHorizontal * speed;
+        }
+
+        public void Jump()
+        {
+            verticalPush = 10;
+        }
+    }
+}
---
+++ Assets/Platform/UserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Platform.Hero;
+using UnityEngine;
+
+namespace Platform
+{
+    public class UserInput
+    {
+        public HeroBehaviour hero;
+
+        public UserInput(HeroBehaviour hero)
+        {
+            this.hero = hero;
+        }
+
+        public void Update()
+        {
+            if (Math.Abs(Input.GetAxisRaw("Horizontal")) > 0)
+            {
+                hero.Walk(Input.GetAxisRaw("Horizontal") / Math.Abs(Input.GetAxisRaw("Horizontal")));
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                hero.Jump();
+            }
+        }
+    }
+}
