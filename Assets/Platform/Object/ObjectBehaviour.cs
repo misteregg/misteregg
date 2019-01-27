6a1426d53ffefa5aed0d8343192648fec82c5e3f
SparK-Cruz <spark.crz@gmail.com>
2019-01-26 23:10:50 -0200

    Add camera following the player

--- Assets/Platform/Object/ObjectBehaviour.cs
+++ Assets/Platform/Object/ObjectBehaviour.cs
@@ -6,9 +6,19 @@ namespace Object
     {
         public GameObject interactButton;
 
+        private int fois = 0;
+
         private void Start()
         {
             interactButton.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (interactButton.activeSelf
+                && Input.GetButtonDown("Fire1")) {
+                Interact();
+            }
         }
 
         void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +29,11 @@ namespace Object
         void OnTriggerExit2D(Collider2D collision)
         {
             interactButton.SetActive(false);
+        }
+
+        void Interact()
+        {
+            Debug.Log("FOI " + (fois++));
         }
     }
 }
